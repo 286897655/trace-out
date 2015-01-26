28e01a92ab053eae41c5291f54de39a11ee536d4
shrpnsld <anton.degtiar@gmail.com>
2015-01-26 23:21:20 +0200

    Raw version of indentation in TLS

--- pretty_output.cpp
+++ pretty_output.cpp
@@ -1,27 +1,63 @@
+#include <assert.h>
+
 #include "pretty_output.h"
 
 
 namespace pretty_output
 {
 
-	std::string _indentation = "";
+	static pthread_once_t once_control = PTHREAD_ONCE_INIT;
+	static pthread_key_t indentation_pthread_key;
+
+
+	void _make_indentation_key()
+	{
+		int retval = pthread_key_create(&indentation_pthread_key, NULL);
+		assert(retval == 0);
+	}
 
 
 	const std::string &indentation()
 	{
-		return _indentation;
+		pthread_once(&once_control, _make_indentation_key);
+
+		std::string *indentation = (std::string*)pthread_getspecific(indentation_pthread_key);
+		if (indentation == NULL)
+		{
+			indentation = new std::string("");
+		}
+
+		return *indentation;
 	}
 
 
 	void indentation_add()
 	{
-		_indentation += INDENTATION;
+		pthread_once(&once_control, _make_indentation_key);
+
+		std::string *indentation = (std::string*)pthread_getspecific(indentation_pthread_key);
+		if (indentation == NULL)
+		{
+			indentation = new std::string("");
+		}
+
+		int retval = pthread_setspecific(indentation_pthread_key, new std::string(*indentation + INDENTATION));
+		assert(retval == 0);
 	}
 
 
 	void indentation_remove()
 	{
-		_indentation = _indentation.substr(0, _indentation.length() - INDENTATION_SIZE);
+		pthread_once(&once_control, _make_indentation_key);
+
+		std::string *indentation = (std::string*)pthread_getspecific(indentation_pthread_key);
+		if (indentation == NULL)
+		{
+			indentation = new std::string("");
+		}
+
+		int retval = pthread_setspecific(indentation_pthread_key, new std::string(indentation->substr(0, indentation->length() - INDENTATION_SIZE)));
+		assert(retval == 0);
 	}
 
 }
