60d20294c63ac6542f0cba3921ee36fba7f672ae
shrpnsld <anton.degtiar@gmail.com>
2015-01-28 00:36:24 +0200

    tls class in pretty_output namespace

--- thread_local_storage.h
+++ thread_local_storage.h
@@ -2,50 +2,55 @@
 #include <assert.h>
 
 
-template <typename T>
-class tls
+namespace pretty_output
 {
-public:
-	tls()
+
+	template <typename T>
+	class tls
 	{
-		int retval = pthread_key_create(&_key, NULL);
-		assert(retval == 0);
-	}
-
-
-	~tls()
-	{
-	}
-
-
-	void set(const T &value)
-	{
-		T *old_value = (T*)pthread_getspecific(_key);
-		if (old_value != NULL)
+	public:
+		tls()
 		{
-			delete old_value;
+			int retval = pthread_key_create(&_key, NULL);
+			assert(retval == 0);
 		}
 
-		T *new_value = new T(value);
-		int retval = pthread_setspecific(_key, new_value);
-		assert(retval == 0);
-	}
 
-
-	T &get() const
-	{
-		T *value = (T*)pthread_getspecific(_key);
-		if (value == NULL)
+		~tls()
 		{
-			value = new T;
-			pthread_setspecific(_key, value);
 		}
 
-		return *value;
-	}
+
+		void set(const T &value)
+		{
+			T *old_value = (T*)pthread_getspecific(_key);
+			if (old_value != NULL)
+			{
+				delete old_value;
+			}
+
+			T *new_value = new T(value);
+			int retval = pthread_setspecific(_key, new_value);
+			assert(retval == 0);
+		}
 
 
-private:
-	pthread_key_t _key;
-};
+		T &get() const
+		{
+			T *value = (T*)pthread_getspecific(_key);
+			if (value == NULL)
+			{
+				value = new T;
+				pthread_setspecific(_key, value);
+			}
+
+			return *value;
+		}
+
+
+	private:
+		pthread_key_t _key;
+	};
+
+}
 
