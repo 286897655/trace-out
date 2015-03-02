7ac4d015557ce84b87520b4599af528838b4b622
shrpnsld <anton.degtiar@gmail.com>
2015-03-02 23:23:42 +0200

    Using only WIDTH value in the field_traits; fixed bug with displaying alternative base signess for decimal base

--- pretty_output.cpp
+++ pretty_output.cpp
@@ -82,10 +82,9 @@ namespace pretty_output
 	};
 
 
-	endl_t endl;
 
 	tls<std::string> _indentation;
-	std::uint64_t _current_thread_id;
+	uint64_t _current_thread_id;
 	tls<std::string> _thread_name;
 	mutex _output_mutex;
 
