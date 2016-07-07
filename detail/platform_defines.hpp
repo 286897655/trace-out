9eb1bc72bcd896c34a41a7667b32a08d7de75ce4
shrpnsld <anton.degtiar@gmail.com>
2016-07-07 16:02:04 +0300

    Added support for MinGW

--- detail/platform_defines.hpp
+++ detail/platform_defines.hpp
@@ -9,11 +9,16 @@
 #if defined(__clang__)
 	#define TRACE_OUT_CLANG
 	#define TRACE_OUT_POSIX
+#elif defined(__MINGW32__)
+	#define TRACE_OUT_MINGW
+	#define TRACE_OUT_WINDOWS
 #elif defined(__GNUC__) || defined(__GNUG__)
 	#define TRACE_OUT_GCC
 	#define TRACE_OUT_POSIX
 #elif defined(_MSC_VER)
 	#define TRACE_OUT_MVS
 	#define TRACE_OUT_WINDOWS
+#else
+	#error Failed to detect compiler and platform
 #endif
 
--- detail/windows/stdlib_specific.cpp
+++ detail/windows/stdlib_specific.cpp
@@ -9,6 +9,19 @@
 namespace trace_out { namespace detail
 {
 
+#if defined(TRACE_OUT_MVS)
+
+	int vsnprintf(char *buffer, size_t size, const char *format, va_list arguments);
+
+#endif // defined(TRACE_OUT_MVS)
+
+}
+}
+
+
+namespace trace_out { namespace detail
+{
+
 	size_t printf_string_length(const char *format, va_list arguments)
 	{
 		int retval = _vscprintf(format, arguments);
@@ -18,9 +31,19 @@ namespace trace_out { namespace detail
 
 	size_t printf_to_string(char *buffer, size_t size, const char *format, va_list arguments)
 	{
-		int retval = _vsnprintf_s(buffer, size, _TRUNCATE, format, arguments);
+		int retval = vsnprintf(buffer, size, format, arguments);
 		return static_cast<size_t>(retval);
 	}
+
+
+#if defined(TRACE_OUT_MVS)
+
+	int vsnprintf(char *buffer, size_t size, const char *format, va_list arguments)
+	{
+		return _vsnprintf_s(buffer, size, _TRUNCATE, format, arguments);
+	}
+
+#endif // defined(TRACE_OUT_MVS)
 
 }
 }
